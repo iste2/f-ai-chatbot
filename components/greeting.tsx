94e5e063234911868fd51c19874c84e2be8c67f9
lnadenau <lukas.nadenau@inform-software.com>
2025-06-12 11:48:31 +0200

    feat: update greeting and suggested actions for improved user interaction

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         transition={{ delay: 0.5 }}
         className="text-2xl font-semibold"
       >
-        Hello there!
+        Hi, I&apos;m Felios.
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
@@ -22,7 +22,7 @@ export const Greeting = () => {
         transition={{ delay: 0.6 }}
         className="text-2xl text-zinc-500"
       >
-        How can I help you today?
+        How can I help with your projects?
       </motion.div>
     </div>
   );
